12789091f9f15481a9a6bb9f9bc987f1577c55f3
Calvin Ho <CalvinHoEmail@gmail.com>
2017-02-09 00:13:58 -0800

    Reverting commit 26 due to lack of issue ID

--- src/clarity/graphics/Map.java
+++ src/clarity/graphics/Map.java
@@ -1,35 +1,5 @@
 package clarity.graphics;
 
 public class Map {
-    private int width;
-    private int height;
 
-    /**
-     * Absolute path to file that map is stored in.
-     */
-    public String mapFile;
-
-    public Map(int mapWidth, int mapHeight, String mapFile) {
-        this.width = mapWidth;
-        this.height = mapHeight;
-        this.mapFile = mapFile;
-    }
-
-    public void setWidth(int width) {
-        if (width >= 0) {
-            this.width = width;
-        }
-        else {
-            throw new IllegalArgumentException("Specified width less than 0.");
-        }
-    }
-
-    public void setHeight(int width) {
-        if (width >= 0) {
-            this.width = width;
-        }
-        else {
-            throw new IllegalArgumentException("Specified height less than 0.");
-        }
-    }
 }
--- src/clarity/graphics/tile/Tile.java
+++ src/clarity/graphics/tile/Tile.java
@@ -1,8 +1,5 @@
 package clarity.graphics.tile;
 
 public class Tile {
-    public int startX;
-    public int startY;
-    public int width;
-    public int height;
+
 }
