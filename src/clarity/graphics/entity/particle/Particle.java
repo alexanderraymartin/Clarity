7dacf27de95ba0eab41938bffbf05cf6894db737
Wen <wenmin.he518@gmail.com>
2017-02-21 12:30:25 -0800

    Task #639108 - Creating methods for particle movements

--- src/clarity/graphics/entity/particle/Particle.java
+++ src/clarity/graphics/entity/particle/Particle.java
@@ -1,5 +1,34 @@
 package clarity.graphics.entity.particle;
 
-public class Particle {
+import clarity.graphics.entity.Entity;
+import clarity.graphics.entity.Sprite;
 
+public class Particle extends Entity {
+  double xpos;
+  double ypos;
+  Sprite sprite;
+  int speed;
+  
+  public Particle(double xpos, double ypos, Sprite sprite, int speed) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.sprite = sprite;
+    this.speed = speed;
+  }
+
+  public double getX() {
+    return xpos;
+  }
+
+  public double getY() {
+    return ypos;
+  }
+
+  public Sprite getSprite() {
+    return sprite;
+  }
+
+  public int getSpeed() {
+    return speed;
+  }
 }
--- src/clarity/graphics/entity/particle/ParticleSpawner.java
+++ src/clarity/graphics/entity/particle/ParticleSpawner.java
@@ -1,5 +1,19 @@
 package clarity.graphics.entity.particle;
 
-public class ParticleSpawner {
+import java.util.ArrayList;
 
+public class ParticleSpawner {
+  ArrayList<Particle> particlearray;
+
+  public ParticleSpawner() {
+    particlearray = new ArrayList<Particle>();
+  }
+
+  public ArrayList<Particle> getArray() {
+    return particlearray;
+  }
+
+  public void addParticle(Particle particle) {
+    particlearray.add(particle);
+  }
 }
