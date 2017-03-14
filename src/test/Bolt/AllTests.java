8587ea12c050be16c64f9d7556e8e6397c35eb6c
Dustin <Dustin@DustinDesktop>
2017-03-14 06:31:18 -0700

    Task #645002 - Add Tests

---
+++ src/test/Bolt/AllTests.java
@@ -0,0 +1,11 @@
+package test.Bolt;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ testSkeleton.class, testSprite.class })
+public class AllTests {
+
+}
