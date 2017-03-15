f9e91e21db56c30d243866db7d0d61d9a50260af
Calvin Ho <CalvinHoEmail@gmail.com>
2017-03-14 21:47:21 -0700

    Task #645002 - Add Tests

--- src/test/ho/ProjectileSuite.java
+++ src/test/ho/ProjectileSuite.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({TestMap.class})
-public class SaveSuite {
+@SuiteClasses({TestBone.class})
+public class ProjectileSuite {
+
 }
---
+++ src/test/ho/TestBone.java
@@ -0,0 +1,30 @@
+package test.ho;
+
+import static org.junit.Assert.assertEquals;
+
+import clarity.graphics.entity.MobId;
+import clarity.graphics.entity.projectile.Bone;
+import org.junit.Test;
+
+/**
+ * Test written by Calvin Ho.
+ */
+public class TestBone {
+
+  @Test
+  public void testBoneNotNull() {
+    Bone testBone = new Bone(MobId.getEntity(MobId.SKELETON), MobId.DUMMY);
+    assert (testBone != null);
+  }
+
+  @Test
+  public void testBoneParticleSpawner() {
+    Bone testBone = new Bone(MobId.getEntity(MobId.SKELETON), MobId.DUMMY);
+    testBone.setEnergy(50);
+    int boneEnergy = (int) testBone.getEnergy();
+
+    assertEquals(50, boneEnergy);
+  }
+
+}
+
