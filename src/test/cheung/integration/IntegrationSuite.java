7aee110b2cb0b66d80989035a03d47ce022f3a57
lzcheung <lzcheung@10.151.122.66>
2017-03-14 22:22:35 -0700

    Task #645002 - Add Tests

---
+++ src/test/cheung/integration/IntegrationSuite.java
@@ -0,0 +1,13 @@
+package test.cheung.integration;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+// Liang Cheung
+
+@RunWith(Suite.class)
+@SuiteClasses({TestHelpToHelpControl.class, TestTorchToSpriteSheet.class})
+public class IntegrationSuite {
+
+}
