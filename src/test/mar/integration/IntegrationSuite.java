adefe9a7be9768bc6491de87d184d45fc07a2662
Jason <Jason@10.151.1.89>
2017-03-14 22:47:34 -0700

    Task #645002 - Add Tests

--- src/test/mar/integration/IntegrationSuite.java
+++ src/test/mar/integration/IntegrationSuite.java
@@ -4,6 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+// Jason Mar
+
 @RunWith(Suite.class)
 @SuiteClasses({TestMobIdToGhost.class, TestMobIdToZombie.class})
 public class IntegrationSuite {
