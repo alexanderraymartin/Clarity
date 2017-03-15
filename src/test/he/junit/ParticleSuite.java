5444f9d3ad2303c5e8bff4bcfa504e4603a7029a
Wen <wenmin.he518@gmail.com>
2017-03-14 22:45:32 -0700

    Task #645002 - Add Tests

--- src/test/he/junit/ParticleSuite.java
+++ src/test/he/junit/ParticleSuite.java
@@ -4,6 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+/**
+ * Test written by Wenmin He.
+ */
+
 @RunWith(Suite.class)
 @SuiteClasses({TestParticle.class, TestParticleSpawner.class})
 public class ParticleSuite {
