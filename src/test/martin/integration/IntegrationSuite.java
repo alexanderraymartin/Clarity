99c085a49734e8763e94beb765ac73ee39ef2480
Alex Martin <alexanderraymartin@gmail.com>
2017-03-14 21:28:09 -0700

    Task #645002 - Add Tests

---
+++ src/test/martin/integration/IntegrationSuite.java
@@ -0,0 +1,11 @@
+package test.martin.integration;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TestLevelToPlayer.class, TestStateManagerToLevel.class})
+public class IntegrationSuite {
+
+}
