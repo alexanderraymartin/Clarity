7738468a9e0120eedb3b86ecc9f61aec682a2dc2
Wen <wenmin.he518@gmail.com>
2017-03-14 20:25:27 -0700

    Task #645002 - Add Tests

--- src/test/loop/SpriteSheetTest.java
+++ src/test/loop/SpriteSheetTest.java
@@ -9,9 +9,30 @@ import org.junit.Test;
 public class SpriteSheetTest {
 
   @Test
-  public void testConstructor() {
-    int [] values = new int[]{1, 1, 1, 1, 1, 1}; 
-    SpriteSheet ss = new SpriteSheet(30, 30, values, "/textures/characters/player.png" );
-    assertEquals(30, ss.getHeight(), 0);
+  public void testNeverExecute() {
+    int [] values = new int[]{}; 
+    SpriteSheet ss = new SpriteSheet(1, 1, values, "/textures/characters/player.png" );
+    assertEquals(1, ss.getHeight(), 0);
+  }
+  
+  @Test
+  public void testExecuteOnce() {
+    int [] values = new int[]{1}; 
+    SpriteSheet ss = new SpriteSheet(1, 1, values, "/textures/characters/player.png" );
+    assertEquals(1, ss.getHeight(), 0);
+  }
+  
+  @Test
+  public void testExecuteTwice() {
+    int [] values = new int[]{1,1}; 
+    SpriteSheet ss = new SpriteSheet(1, 1, values, "/textures/characters/player.png" );
+    assertEquals(1, ss.getHeight(), 0);
+  }
+  
+  @Test
+  public void testExecuteFiveTimes() {
+    int [] values = new int[]{1,1,1,1,1}; 
+    SpriteSheet ss = new SpriteSheet(1, 1, values, "/textures/characters/player.png" );
+    assertEquals(1, ss.getHeight(), 0);
   }
 }
